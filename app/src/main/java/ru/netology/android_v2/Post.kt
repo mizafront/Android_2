a515069517d9c644e653275218346c45ae081ac2
Кирилл <48541690+mizafront@users.noreply.github.com>
2021-02-09 21:25:01 +0500

    Update Post.kt

--- app/src/main/java/ru/netology/android_v2/Post.kt
+++ app/src/main/java/ru/netology/android_v2/Post.kt
@@ -1,13 +1,10 @@
 package ru.netology.android_v2
 
 data class Post (
-    val id: Int,
+    val id: Long,
     val author: String,
     val published: String,
     val content: String,
     val liked: Boolean = false,
     val likesCount: Int = 0,
-    val shared: Boolean = false,
-    val shareCount: Int = 0,
-    val viewCount: Int = 0
 )
